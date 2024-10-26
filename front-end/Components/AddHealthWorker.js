158c29ddcaa5b976596720792270dfecc1f7d82b
Phindu208 <phnestifhefhe208@gmail.com>
2024-10-26 18:45:12 +0200

    package.json updates

---
+++ front-end/Components/AddHealthWorker.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const AddHealthWorker= () => {
+    return (
+        <View>
+            <Text>Hello Worldooo</Text>
+        </View>
+    );
+};
+
+export default AddHealthWorker;
---
+++ front-end/app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+import React, { useEffect, useState } from 'react';
+import AddHealthWorker from '../Components/AddHealthWorker';
+export default function Index() {
+
+    return (
+        <View>
+            <AddHealthWorker />
+        </View>
+    );
+}
