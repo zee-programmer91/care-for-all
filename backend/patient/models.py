821ac97bb8c396bb56ae92e28b49c6bc6ed91f30
Zwelisha Brian Langa <81659005+zee-programmer91@users.noreply.github.com>
2024-10-26 11:42:44 +0200

    Created And Added Patient Models

--- backend/patient/models.py
+++ backend/patient/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+class Patient(models.Model):
+  idNumber = models.CharField(max_length=13)
+  firstName = models.CharField(max_length=255)
+  lastName = models.CharField(max_length=255)
+  dateOfBirth = models.DateField
+  gender = models.CharField(max_length=1)
+
+class ContactInformation(models.Model):
+    idNumber = models.CharField(max_length=13)
+    phoneNumber = models.CharField(max_length=15)
+    emailAddress = models.CharField(max_length=100)
+
+class Address(models.Model):
+    idNumber = models.CharField(max_length=13)
+    addressLine1 = models.CharField(max_length=255)
+    addressLine2 = models.CharField(max_length=255)
+    city = models.CharField(max_length=50)
+    postalCode = models.CharField(max_length=4)
+    
+class MedicalHistory(models.Model):
+      idNumber = models.CharField(max_length=13)
+      chronicCondition = models.CharField(max_length=255)
+      allergies = models.CharField(max_length=3000)
+      medications = models.CharField(max_length=3000)
+      
+class EmergencyContact(models.Model):
+    idNumber = models.CharField(max_length=13)
+    firstName = models.CharField(max_length=255)
+    lastName = models.CharField(max_length=255)
